6b213d316c8cb5002cb16abadea40c8609e1d22a
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-08-22 22:49:10 -0700

    restyled userPosts component

--- src/components/UserPosts/UserPosts.js
+++ src/components/UserPosts/UserPosts.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { fetchCreatedPosts } from "../../apiCalls";
-// import "./css/style.css";
+import "./UserPosts.css";
 
 const UserPosts = (props) => {
     const { token } = props;
@@ -45,38 +45,42 @@ const UserPosts = (props) => {
 
     return (
         <>
-            <form className="container" onSubmit={submitHandler}>
-                <h2 className="center">Tell us what you're selling</h2>
-                <div className="center">
+            <form className="container userPostsForm" onSubmit={submitHandler}>
+                <h2 className="centerUserPost">Tell us what you're selling</h2>
+                <div className="centerUserPost">
                     <input 
+                    className="userPostsInput"
                     placeholder=" Title"
                     value={title}
                     onChange={titleChangeHandler}
                     />
                 </div>
-                <div className="center">
+                <div className="centerUserPost">
                     <input 
+                    className="userPostsInput"
                     placeholder=" Description"
                     value={description}
                     onChange={descriptionChangeHandler}
                     />
                 </div>
-                <div className="center">
+                <div className="centerUserPost">
                     <input 
+                    className="userPostsInput"
                     placeholder=" Price"
                     value={price}
                     onChange={priceChangeHandler}
                      />
                 </div>
-                <div className="center">
+                <div className="centerUserPost">
                     <input 
+                    className="userPostsInput"
                     placeholder=" Location"
                     value={location}
                     onChange={locationChangeHandler}
                      />
                 </div>
-                <div className="center">
-                    <button className="btn btn-lg btn-primary">Create Listing</button>
+                <div className="centerUserPost">
+                    <button className="btn btn-lg btn-primary listingBtn">Create Listing</button>
                 </div>
             </form>
         </>
