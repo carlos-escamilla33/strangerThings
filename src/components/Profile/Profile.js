b0c220dafb35d2b45248efbd87d04c31155fd358
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-08-28 20:04:13 -0700

    refactored Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -29,31 +29,13 @@ const Profile = (props) => {
 
         messages.length !== 0 ?
             <div className="container">
-                <h1 className="text-center display-3">My Posts:</h1>
 
-                {
-                    posts.map(post => {
-                        return (
-                            <div className="card border-dark text-center postSpacing" key={post._id}>
-                                <h2 className="card-header">Title: {post.title}</h2>
-                                <div className="card-body">
-                                    <blockquote className="blockquote mb-0">
-                                        <p>Description: {post.description}</p>
-                                        <p>Price: {post.price}</p>
-                                        <p>Location: {post.location}</p>
-                                    </blockquote>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-
-                <h1 className="text-center display-3">Messages:</h1>
+                <h1 className="text-center display-3">messages recieved:</h1>
 
                 {
                     messages.map(message => {
                         return (
-                             message.fromUser.username !== user ?
+                            message.fromUser.username !== user ?
                                 <div className="card text-center border-danger postSpacing" key={message._id}>
                                     <h2 className="card-header">from: {message.fromUser.username}</h2>
                                     <div className="card-body">
@@ -62,8 +44,18 @@ const Profile = (props) => {
                                             <p>Content: {message.content}</p>
                                         </blockquote>
                                     </div>
-                                </div> 
-                                : 
+                                </div>
+                                :
+                                null
+                        )
+                    })
+                }
+
+                <h1 className="text-center display-3">messages sent:</h1>
+                {
+                    messages.map(message => {
+                        return (
+                            message.fromUser.username !== user ?
                                 <div className="card text-center border-primary postSpacing" key={message._id}>
                                     <h2 className="card-header">(sent by me)</h2>
                                     <div className="card-body">
@@ -73,6 +65,8 @@ const Profile = (props) => {
                                         </blockquote>
                                     </div>
                                 </div>
+                                :
+                                null
                         )
                     })
                 }
